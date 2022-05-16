452ee37f7e1a06c4233c3f9ab7d207bb637aca00
Kevin Vargas <keving.vargasb@gmail.com>
2022-05-16 16:45:52 -0500

    more buttons

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -17,13 +17,20 @@ function Counter() {
     const reset = () => {
         setCount(0)
     }
-    
+
+    function random(min, max) {
+        const number= Math.floor((Math.random() * (max - min + 1)) + min);   
+          setCount(number)
+    }
+
     return (
         <div>
             <h1>{count}</h1>
             <button type="button" onClick={() => increase()}>plus</button>
             <button type="button" onClick={() => decrease()}>minus</button>
             <button type="button" onClick={() => reset()}>reset</button>
+            <button type="button" onClick={() => random(1, 100)}>random</button>
+            <button type="button" onClick={() => random(1000, 2000)}>more random</button>
         </div>
     );
 }
